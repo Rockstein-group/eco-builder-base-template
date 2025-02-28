b36e3be2fb8895f5a02454d9aa99202bbd123173
Johann Taberlet <johann.taberlet@gmail.com>
2025-02-28 13:51:23 +0100

    feat: added tailwind and removed Astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,21 @@ import { defineConfig } from "astro/config";
 
 import node from "@astrojs/node";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+
   output: "server",
+
   adapter: node({
     mode: "standalone",
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
